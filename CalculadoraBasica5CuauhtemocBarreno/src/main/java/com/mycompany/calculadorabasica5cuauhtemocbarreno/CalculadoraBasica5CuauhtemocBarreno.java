b37f5e6217ef22c492473e0257f15c0359a237d7
21cbs05 <c.barreno.5adb@gmail.com>
2026-04-17 13:00:51 -0600

    Add files via upload

---
+++ CalculadoraBasica5CuauhtemocBarreno/src/main/java/com/mycompany/calculadorabasica5cuauhtemocbarreno/CalculadoraBasica5CuauhtemocBarreno.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadorabasica5cuauhtemocbarreno;
+
+/**
+ *
+ * @author LABORATORIO
+ */
+public class CalculadoraBasica5CuauhtemocBarreno {
+
+    public static void main(String[] args) {
+        jfrmCalc holaPos=new jfrmCalc();
+        holaPos.setVisible(true);
+    }
+}
