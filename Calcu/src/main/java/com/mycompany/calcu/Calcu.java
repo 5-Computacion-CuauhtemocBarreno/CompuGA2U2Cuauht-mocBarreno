012ac3eaaed883413fae40e15343ebac5bbec5df
21cbs05 <c.barreno.5adb@gmail.com>
2026-04-23 23:33:06 -0600

    Add files via upload

---
+++ Calcu/src/main/java/com/mycompany/calcu/Calcu.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calcu;
+
+/**
+ *
+ * @author Zempo
+ */
+public class Calcu {
+
+    public static void main(String[] args) {
+        Calc holaPos=new Calc();
+        holaPos.setVisible(true);
+    }
+}
